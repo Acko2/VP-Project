1ab11cd647a9557e341b5ca8e17c3a19f8d763ab
Petar <bozinovski.k4@gmail.com>
2020-07-04 17:55:33 +0200

    Empty string verification, adding back button

--- quiz/startForm.Designer.cs
+++ quiz/startForm.Designer.cs
@@ -35,6 +35,7 @@
             this.btnExit = new System.Windows.Forms.Button();
             this.txtName = new System.Windows.Forms.TextBox();
             this.lblName = new System.Windows.Forms.Label();
+            this.btnBack = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblHeader
@@ -122,6 +123,22 @@
             this.lblName.TabIndex = 5;
             this.lblName.Text = "Enter your name";
             // 
+            // btnBack
+            // 
+            this.btnBack.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnBack.FlatAppearance.BorderColor = System.Drawing.Color.CornflowerBlue;
+            this.btnBack.FlatAppearance.BorderSize = 3;
+            this.btnBack.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBack.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnBack.Location = new System.Drawing.Point(32, 376);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(119, 41);
+            this.btnBack.TabIndex = 6;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = false;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
             // startForm
             // 
             this.AcceptButton = this.btnStart;
@@ -131,6 +148,7 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnBack);
             this.Controls.Add(this.lblName);
             this.Controls.Add(this.txtName);
             this.Controls.Add(this.btnExit);
@@ -158,5 +176,6 @@
         private System.Windows.Forms.Button btnExit;
         private System.Windows.Forms.TextBox txtName;
         private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Button btnBack;
     }
 }
